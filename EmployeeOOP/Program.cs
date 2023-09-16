91c6b99b9d374670e6b1674b8ff3cfb22b64b37c
Laura Escobar <lauraer442@gmail.com>
2023-09-16 15:44:34 -0500

    This is a new advance.

--- EmployeeOOP/Program.cs
+++ EmployeeOOP/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EmployeeOOP.Classes;
+
+try
+{
+    Console.WriteLine("OOP APPLICATION ");
+    Console.WriteLine("-----------------");
+
+    //Inicialización de la clase Date.
+    Date dataObject = new Date(4, 18, 5734834);
+    Console.WriteLine(dataObject.ToString());
+    Console.ReadKey();
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
